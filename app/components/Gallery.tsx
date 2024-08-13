1bc396bce74ffb8ae9a6d76cbfbe300feb52a5a4
Matthew Ranford <matt.ranford16@gmail.com>
2024-08-13 13:54:16 +1200

    Initial layout and responsiveness of gallery

--- app/components/Gallery.tsx
+++ app/components/Gallery.tsx
@@ -18,16 +18,16 @@ export default function Gallery() {
 
   return (
     <>
-      <main id="gallery" className="pb-10 mt-10 bg-zinc-900">
+      <main id="gallery" className="mt-10 bg-zinc-900">
         <div className="lg:flex lg:justify-end">
-          <div className="pt-8 lg:pt-36 bigger-screens:pt-[40rem] pb-6">
-            <h1 className="text-left text-amber-600 text-5xl lg:text-7xl 2xl:text-8xl bigger-screens:text-9xl leading-none tracking-tight px-6 lg:px-0 lg:pe-32 2xl:pe-28 bigger-screens:pe-4 block bigger-screens:inline-block">
+          <div className="pt-8 lg:pt-36 2xl:pt-52 bigger-screens:pt-96 pb-6">
+            <h1 className="text-left text-amber-600 text-5xl lg:text-7xl 2xl:text-8xl bigger-screens:text-9xl leading-none tracking-tight px-6 lg:px-0 lg:pe-32 2xl:pe-52 bigger-screens:pe-10 block bigger-screens:inline-block">
               Our{' '}
               <span className="lg:block bigger-screens:inline-block">
                 Work!
               </span>
             </h1>
-            <p className="text-left pt-10 px-8 lg:px-0 2xl:px-6 pb-4 text-xl 2xl:text-2xl bigger-screens:text-3xl leading-none tracking-tight text-balance lg:max-w-xs 2xl:max-w-sm bigger-screens:max-w-2xl block">
+            <p className="text-left text-zinc-300 pt-10 px-8 lg:px-0 2xl:px-6 pb-4 text-xl 2xl:text-2xl bigger-screens:text-3xl leading-none tracking-tight text-balance lg:max-w-sm 2xl:max-w-xl bigger-screens:max-w-2xl block">
               Whether you&apos;re looking for inspiration or seeking the perfect
               design to elevate your home!{' '}
               <span className="block pt-4">
