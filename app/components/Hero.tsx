89067dbfc9c0a065bdedca3c1aec3b103b1d9772
Matthew Ranford <matt.ranford16@gmail.com>
2024-08-15 18:22:21 +1200

    Add flip words to UI and test in hero section

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,8 +1,11 @@
 'use client'
 
-import { titan } from '@/fonts'
+import { titan, interHeroText } from '@/fonts'
+import { FlipWords } from '@/components/ui/flip-words'
 
 export default function Hero() {
+  const words = ['Space', 'Home', 'Area', 'Walls']
+
   return (
     <>
       <main className="hero-wrapper">
@@ -25,26 +28,28 @@ export default function Hero() {
           <div className="mt-44 lg:mt-52 2xl:mt-72 lg:max-w-6xl 2xl:max-w-screen-2xl px-6 lg:px-28 2xl:px-20 bigger-screens:px-60">
             <div className={titan.className}>
               <h1 className="text-5xl lg:text-7xl 2xl:text-8xl bigger-screens:text-9xl leading-none tracking-tight">
-                Transform Your Space with Elegance
+                Transform Your <FlipWords words={words} /> with Elegance
               </h1>
             </div>
           </div>
           <div className="text-xl 2xl:text-2xl bigger-screens:text-3xl leading-none tracking-tight pt-8 bigger-screens:pt-16 px-6 lg:px-[7.5rem] 2xl:px-20 bigger-screens:px-64 max-w-3xl bigger-screens:max-w-7xl">
-            <p className="pe-4">
-              At <span className="text-amber-600">Strictly Paper</span>, we
-              specialize in turning walls into works of art.
-            </p>
-            <p className="pt-4 pe-2">
-              With our expert wallpapering services, we bring your vision to
-              life, creating stunning, unique interiors that reflect your style
-              and personality.
-            </p>
+            <div className={interHeroText.className}>
+              <p className="pe-4">
+                At <span className="text-amber-600">Strictly Paper</span>, we
+                specialize in turning walls into works of art.
+              </p>
+              <p className="pt-4 pe-2">
+                With our expert wallpapering services, we bring your vision to
+                life, creating stunning, unique interiors that reflect your
+                style and personality.
+              </p>
+            </div>
             <div className="pt-6 2xl:pt-10 bigger-screens:pt-16">
               <div className={titan.className}>
-                <button className="border-2 rounded-xl border-zinc-950 bg-amber-600 transition-all duration-1000 hover:bg-zinc-950 hover:text-amber-600 px-3 py-3">
+                <button className="border-2 rounded-xl border-zinc-950 bg-amber-600 transition-all duration-1000 hover:bg-zinc-950 hover:text-amber-600 py-3 px-3 sm:px-4 sm:py-4">
                   <a href="#about">Who we Are</a>
                 </button>
-                <button className="border-2 rounded-xl border-zinc-950 bg-amber-600 transition-all duration-1000 hover:bg-zinc-950 hover:text-amber-600 px-3 py-3 mx-3 lg:mx-4">
+                <button className="border-2 rounded-xl border-zinc-950 bg-amber-600 transition-all duration-1000 hover:bg-zinc-950 hover:text-amber-600 py-3 px-3 sm:px-4 sm:py-4 mx-3 lg:mx-4">
                   <a href="#contact">Get a Quote</a>
                 </button>
               </div>
--- fonts.ts
+++ fonts.ts
@@ -3,5 +3,8 @@ import { Titan_One, Inter } from 'next/font/google'
 // Header text
 export const titan = Titan_One({ subsets: ['latin'], weight: ['400'] })
 
+// Hero paragraph text
+export const interHeroText = Inter({ subsets: ['latin'], weight: ['500'] })
+
 // Main body text
 export const inter = Inter({ subsets: ['latin'], weight: ['400'] })
