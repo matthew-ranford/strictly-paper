d3db7b6d2f3c3c83a15c2d712e6781128ef96027
Matthew Ranford <matt.ranford16@gmail.com>
2024-08-18 10:03:20 +1200

    Re-adjust navbar motion animation

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react'
 import { titan } from '@/fonts'
-import { motion, animate, stagger } from 'framer-motion'
+import { motion } from 'framer-motion'
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false)
@@ -22,9 +22,17 @@ export default function Navbar() {
     setIsOpen(!isOpen)
   }
 
-  const sequence = [
-    ['li', { opacity: [0, 1], x: [100, 0] }, { delay: stagger(0.15) }],
-  ]
+  const navItemVariants = {
+    hidden: { opacity: 0, x: 200 },
+    visible: (i: number) => ({
+      opacity: 1,
+      x: 0,
+      transition: {
+        delay: i * 0.15,
+        stagger: 0.1,
+      },
+    }),
+  }
 
   return (
     <>
@@ -48,33 +56,40 @@ export default function Navbar() {
             <span className="block">Paper</span>
           </h1>
         </div>
-        <ul className="hidden lg:flex gap-10 text-xl 2xl:text-2xl bigger-screens:text-3xl">
+        <ul className="hidden lg:flex gap-11 bigger-screens:gap-12 text-xl 2xl:text-2xl bigger-screens:text-3xl">
           {rightNavLinks.map((link, index) => (
             <li className="list-none nav-link" key={index}>
               <a href={link.href}>{link.text}</a>
             </li>
           ))}
         </ul>
-        <ul
-          className={`absolute lg:hidden w-full min-h-screen flex flex-col items-right gap-2 py-16 sm:py-8 px-4 text-right text-5xl sm:text-6xl bg-zinc-50 ${
+        <motion.ul
+          className={`absolute lg:hidden w-full min-h-screen flex flex-col items-right gap-3 py-16 sm:py-8 px-4 text-right text-5xl sm:text-6xl bg-zinc-300 ${
             isOpen
               ? 'top-36 animate-slideIn border-4 border-zinc-950 rounded-2xl'
               : 'hidden'
           }`}
+          initial="hidden"
+          animate={isOpen ? 'visible' : 'hidden'}
         >
           {[...leftNavLinks, ...rightNavLinks].map((link, index) => (
-            <div className={titan.className} key={index}>
+            <motion.div
+              className={titan.className}
+              key={index}
+              custom={index}
+              variants={navItemVariants}
+            >
               <li className="list-none py-2">
                 <a href={link.href} onClick={handleMenuClick}>
                   {link.text}
                 </a>
               </li>
-            </div>
+            </motion.div>
           ))}
-        </ul>
+        </motion.ul>
         <div className="lg:hidden flex">
           <button
-            onClick={() => setIsOpen(!isOpen, animate(sequence))}
+            onClick={handleMenuClick}
             aria-label="button"
             aria-controls="navbar-dropdown-menu"
             aria-expanded={isOpen}
@@ -86,11 +101,10 @@ export default function Navbar() {
            isOpen ? 'w-14 rotate-45 translate-y-1.5' : 'w-14 -translate-y-0.5'
          }`}
             ></span>
-
             <span
               className={`bg-zinc-950 block transition-all duration-700 ease-out
          h-1 rounded-2xl ml-auto ${
-           isOpen ? 'w-14 -rotate-45 -translate-y-1.5' : 'w-8 translate-y-0.5'
+           isOpen ? 'w-14 -rotate-45 -translate-y-0.5' : 'w-8 translate-y-0.5'
          }`}
             ></span>
           </button>
