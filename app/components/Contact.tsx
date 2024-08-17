23b902651c7396e3396a8150de40e62e00aff924
Matthew Ranford <matt.ranford16@gmail.com>
2024-08-18 11:06:28 +1200

    Couple of css changes for medium sized screens in Contact component

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -5,22 +5,22 @@ export default function Contact() {
     <>
       <main id="contact" className="bg-zinc-900 pb-10">
         <div className={titan.className}>
-          <h1 className="pt-8 lg:pt-12 text-center text-amber-500 text-5xl lg:text-7xl 2xl:text-8xl bigger-screens:text-9xl leading-none tracking-tight">
+          <h1 className="pt-8 lg:pt-12 text-center text-amber-500 text-5xl md:text-7xl lg:text-7xl 2xl:text-8xl bigger-screens:text-9xl leading-none tracking-tight">
             Get In Touch With Us!
           </h1>
         </div>
         <div className="flex flex-col lg:flex-row items-center place-content-evenly bigger-screens:place-content-center">
-          <div className="pt-10 lg:pt-0 text-balance lg:text-pretty text-center md:text-left max-w-md lg:max-w-lg bigger-screens:max-w-xl bigger-screens:mx-20">
-            <h2 className="text-zinc-300 text-xl 2xl:text-2xl bigger-screens:text-3xl leading-none tracking-tight mb-5">
+          <div className="pt-10 lg:pt-0 text-balance lg:text-pretty text-center md:text-center lg:text-left max-w-md lg:max-w-lg bigger-screens:max-w-xl bigger-screens:mx-20">
+            <h2 className="text-zinc-200 text-xl 2xl:text-2xl bigger-screens:text-3xl leading-tight tracking-tight mb-5">
               Questions? Fill out the form to contact John!
             </h2>
-            <p className="text-zinc-300 text-xl 2xl:text-2xl bigger-screens:text-3xl leading-none tracking-tight mb-5">
+            <p className="text-zinc-200 text-xl 2xl:text-2xl bigger-screens:text-3xl leading-tight tracking-tight mb-5">
               Alternatively, email or call John directly from one of the links
               below!
             </p>
-            <div className="text-amber-500 text-xl 2xl:text-2xl bigger-screens:text-3xl leading-none tracking-tight mb-5">
+            <div className="text-amber-500 text-xl 2xl:text-2xl bigger-screens:text-3xl leading-tight tracking-tight mb-5">
               <a href="mailto:email@gmail.com">john.daken@gmail.com</a>
-              <span className="text-zinc-300 hidden sm:inline"> | </span>
+              <span className="text-zinc-200 hidden sm:inline"> | </span>
               <a
                 href="tel:0274930320"
                 className="pt-4 lg:pt-0 block lg:inline-block"
@@ -28,7 +28,7 @@ export default function Contact() {
                 +64 27 493 0320
               </a>
             </div>
-            <p className="text-zinc-300 text-xl 2xl:text-2xl bigger-screens:text-3xl leading-none tracking-tight mb-5">
+            <p className="text-zinc-200 text-xl 2xl:text-2xl bigger-screens:text-3xl leading-tight tracking-tight mb-5">
               For any painting related enquiries, we recommend
               <a
                 href="https://www.atd-decorators.co.nz"
@@ -39,23 +39,23 @@ export default function Contact() {
                 ATD Decorators
               </a>{' '}
             </p>
-            <p className="text-zinc-300 text-xl 2xl:text-2xl bigger-screens:text-3xl leading-none tracking-tight mb-5">
+            <p className="text-zinc-200 text-xl 2xl:text-2xl bigger-screens:text-3xl leading-tight tracking-tight mb-5">
               We will get back to you as soon as possible!
             </p>
           </div>
-          <div className="pt-10 lg:pt-20">
+          <div className="pt-10 lg:pt-20 md:pe-6 lg:pe-0">
             <div className="relative mb-4">
               <label
                 htmlFor="name"
                 className="leading-7 text-sm text-zinc-300 ps-4"
               >
-                Name*
+                Name<span className="text-amber-500">*</span>
               </label>
               <input
                 type="text"
                 id="name"
                 name="name"
-                className="w-11/12 md:w-full mx-4 bg-white rounded border border-gray-500 focus:border-amber-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-zinc-950 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                className="w-11/12 md:w-full mx-4 bg-zinc-200 rounded border border-gray-500 focus:border-amber-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-zinc-950 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               />
             </div>
             <div className="relative mb-4">
@@ -63,13 +63,13 @@ export default function Contact() {
                 htmlFor="email"
                 className="leading-7 text-sm text-zinc-300 ps-4"
               >
-                Email Address*
+                Email Address<span className="text-amber-500">*</span>
               </label>
               <input
                 type="email"
                 id="email"
                 name="email"
-                className="w-11/12 md:w-full mx-4 bg-white rounded border border-gray-500 focus:border-amber-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-zinc-950 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                className="w-11/12 md:w-full mx-4 bg-zinc-200 rounded border border-gray-500 focus:border-amber-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-zinc-950 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               />
             </div>
             <div className="relative mb-4">
@@ -83,7 +83,7 @@ export default function Contact() {
                 type="phone"
                 id="phone"
                 name="phone"
-                className="w-11/12 md:w-full mx-4 bg-white rounded border border-gray-500 focus:border-amber-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-zinc-950 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                className="w-11/12 md:w-full mx-4 bg-zinc-200 rounded border border-gray-500 focus:border-amber-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-zinc-950 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
               />
             </div>
             <div className="relative mb-4">
@@ -91,12 +91,12 @@ export default function Contact() {
                 htmlFor="message"
                 className="leading-7 text-sm text-zinc-300 ps-4"
               >
-                Message*
+                Message<span className="text-amber-500">*</span>
               </label>
               <textarea
                 id="message"
                 name="message"
-                className="w-11/12 md:w-full mx-4 bg-white rounded border border-gray-500 focus:border-amber-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-zinc-950 py-1 px-3 resize-y leading-6 transition-colors duration-200 ease-in-out"
+                className="w-11/12 md:w-full mx-4 bg-zinc-200  rounded border border-gray-500 focus:border-amber-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-zinc-950 py-1 px-3 resize-y leading-6 transition-colors duration-200 ease-in-out"
               />
             </div>
             <div className="text-center">
