87583b815c1c4d0b9016dabb0647e296033059b2
Matthew Ranford <matt.ranford16@gmail.com>
2024-10-08 17:47:46 +1300

    Test changes again

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: 'https://www.strictly-paper.nz',
+  assetPrefix: 'http://www.strictly-paper.nz',
   images: {
     unoptimized: true,
   },
