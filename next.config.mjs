09d74a52b7f8da1e44449f1c252dfa0e2cfabf17
Matthew Ranford <matt.ranford16@gmail.com>
2024-10-08 18:17:42 +1300

    Further update on next config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: 'http:/strictly-paper.nz',
+  assetPrefix: 'http://strictly-paper.nz',
   images: {
     unoptimized: true,
   },
