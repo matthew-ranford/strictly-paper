c2cee716f8ef7ef517d93c2e1ffff048a078ee18
Matthew Ranford <matt.ranford16@gmail.com>
2025-03-15 13:54:57 +1000

    Push changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Comment this out when working on development in localhost
-  // assetPrefix: 'https://strictly-paper.nz',
+  assetPrefix: 'https://strictly-paper.nz',
 }
 
 export default nextConfig
